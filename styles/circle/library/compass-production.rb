2e8b7a3a0f2a5f6f1e94f7b707280a5b1cca9059
srcspider <source.spider@gmail.com>
2013-08-30 02:06:39 +0300

    first site version

---
+++ styles/circle/library/compass-production.rb
@@ -0,0 +1,14 @@
+require 'sass-globbing' # gem install sass-globbing
+
+# Required Mjolnir configuration
+
+	http_path = ""
+	css_dir = "root"
+	images_dir = "root/images"
+	relative_assets = true
+	sass_dir = "src"
+
+# Output
+
+	output_style = :nested
+	line_comments = false
